22e82ec6ccbfa90746bdd0a5a4238c528ada3ea8
Lucas Månsson <lu5132ma-s@student.lu.se>
2024-12-11 12:20:32 +0100

    La till kodskelett

---
+++ node.cc
@@ -0,0 +1,28 @@
+#include "node.h"
+#include <vector>
+#include <string>
+
+Node::Node(const std::string& name){
+
+}
+/** Hämtar nodens namn. */
+std::string Node::getName() const{
+
+}
+/** Sätter nodens värde till v. */
+void Node::setValue(int v){
+
+}
+/** Hämtar nodens värde. */
+int Node::getValue() const{
+
+}
+/** Lägger till en ny båge från denna nod till en given destination.
+Bågen ska ha längden length. */
+void Node::addEdge(Node* destination, int length){
+
+}
+/** Hämtar de bågar som utgår från denna nod. */
+const std::vector<Edge>& Node::getEdges() const{
+
+}
